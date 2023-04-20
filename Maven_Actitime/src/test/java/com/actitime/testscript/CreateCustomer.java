296f6bc5fdaa5955c82bbf1d38371db575eab7cb
ACER <ACER@Satishraj>
2023-04-20 13:08:58 +0530

    pr

---
+++ Maven_Actitime/src/test/java/com/actitime/testscript/CreateCustomer.java
@@ -0,0 +1,15 @@
+package com.actitime.testscript;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class CreateCustomer {
+	public class CustomerCustomer{
+
+		@Test(priority = 1)
+		public void createCustomer() {
+			Reporter.log("createCustomer", true);
+
+		}
+	}
+}
